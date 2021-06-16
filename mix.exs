ccddc47af1043a0ffe3f2aed8cfd35cd346f939f
Simon McConnell <22566656+simonmcconnell@users.noreply.github.com>
2021-06-16 14:46:01 +1000

    upgrade timex

--- mix.exs
+++ mix.exs
@@ -24,22 +24,17 @@ defmodule ElixirDbf.MixProject do
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger, :timex]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
-      {:timex, "~> 3.6.2"},
-      {:exconv, "~> 0.1.3"},
-
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:timex, "~> 3.7.5"},
+      {:exconv, "~> 0.1.3"}
     ]
   end
 end
